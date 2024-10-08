43db0a466a1f81def8c8001c7f18e6a33883417a
SHR <SHR@SHRs-macbook-air.local>
2024-10-07 22:21:40 -0400

    HW2

--- wildlife_tracker/migration_tracking/migration.py
+++ wildlife_tracker/migration_tracking/migration.py
@@ -1,5 +1,26 @@
 from typing import Any
 
 class Migration:
+    def __init__(self,  current_date: str, migration_id: int,
+        destination: Habitat, 
+         start_location: Habitat, current_location: str, 
+        species: str,
+         path_id: int,
+          start_date: str,
+        status: str = "Scheduled",
+                       
+                        
+                       
+            paths: dict[int, MigrationPath] = {},
+                        duration: Optional[int] = None,
 
-    pass
+       ) -> None:
+        self.current_date =current_date
+        self.start_date = start_date
+        self.migration_id = migration_id
+        pass
+    def get_migration_paths() -> list[MigrationPath]:
+        pass
+  
+   
+    
